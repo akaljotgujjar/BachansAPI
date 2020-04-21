7e68988391083bfaa82c212ebf552bc5eefd3411
saiaungkoko <saiaungkoko@mail.fresnostate.edu>
2020-04-21 12:28:57 -0700

    finish get

--- dist/definitions/models/book.model.d.ts
+++ dist/definitions/models/book.model.d.ts
@@ -1,5 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
 export declare class Book {
     _model: any;
     constructor(norm: any);
+    getAllBooks(model: any): (req: Request<import("../../../../../../../../../Users/aungk/Desktop/Spring 2020/IS 183/LabAssignments/ExamAPI/node_modules/@types/express-serve-static-core").ParamsDictionary, any, any, import("../../../../../../../../../Users/aungk/Desktop/Spring 2020/IS 183/LabAssignments/ExamAPI/node_modules/@types/express-serve-static-core").Query>, res: Response<any>, next: NextFunction) => Promise<void>;
+    getBookById(model: any): (req: Request<import("../../../../../../../../../Users/aungk/Desktop/Spring 2020/IS 183/LabAssignments/ExamAPI/node_modules/@types/express-serve-static-core").ParamsDictionary, any, any, import("../../../../../../../../../Users/aungk/Desktop/Spring 2020/IS 183/LabAssignments/ExamAPI/node_modules/@types/express-serve-static-core").Query>, res: Response<any>, next: NextFunction) => Promise<void>;
     model: any;
 }
